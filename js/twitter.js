60b3f2e90e336c7e5d36b249edf5e1015fc4c2a4
Nathan Chaney <nrchaney@us.ibm.com>
2016-02-15 20:15:22 +0000

    updated copyright

--- js/twitter.js
+++ js/twitter.js
@@ -1,4 +1,7 @@
-// Licensed under the Apache License. See footer for details.
+// Copyright (c) 2016 IBM Corp. All rights reserved.
+// Use of this source code is governed by the Apache License,
+// Version 2.0, a copy of which can be found in the LICENSE file.
+
 var request = require('request');
 
 function Twitter(url) {
@@ -62,16 +65,3 @@ function Twitter(url) {
 module.exports = function (url) {
   return new Twitter(url);
 };
-//------------------------------------------------------------------------------
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//------------------------------------------------------------------------------
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
-// Licensed under the Apache License. See footer for details.
+// Copyright (c) 2016 IBM Corp. All rights reserved.
+// Use of this source code is governed by the Apache License,
+// Version 2.0, a copy of which can be found in the LICENSE file.
 
 // listen for request sent over XHR and automatically show/hide spinner
 (function () {
@@ -33,16 +35,3 @@ var app = angular.module('app', ['ngNumeraljs', 'ngLoadingSpinner']);
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-//------------------------------------------------------------------------------
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//------------------------------------------------------------------------------
